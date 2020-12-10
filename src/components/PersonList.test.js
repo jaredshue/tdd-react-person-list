9a4788df8ea2d3b362c1f9276370f34639b6bcca
jaredshue <74428951+jaredshue@users.noreply.github.com>
2020-12-10 12:59:16 -0500

    Update PersonList.test.js

---
+++ src/components/PersonList.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import PersonList from "./PersonList";
+
+describe("PersonList", () => {
+    it("renders", () => {
+        shallow(<PersonList />);
+    });
+});
