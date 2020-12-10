49bed9ee1c5489a4d927fd3b4f7ff5f1381422b2
jaredshue <74428951+jaredshue@users.noreply.github.com>
2020-12-10 13:02:55 -0500

    Move tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import App from "./App";
-import PersonList from "./components/PersonList";
+import App from "../App";
+import PersonList from "../../components/PersonList";
 
 describe("App", () => {
     var appWrapper;
