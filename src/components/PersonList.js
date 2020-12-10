c8d0793c3da5fb29f955cc7e2208884e98c4d4ce
jaredshue <74428951+jaredshue@users.noreply.github.com>
2020-12-10 13:34:53 -0500

    Update PersonList.js

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,7 +1,21 @@
-function PersonList() {
+function PersonList(props) {
+    const people =
+        props.people
+        && props.people.length !== 0
+        ? props.people.map((person, index) => (
+            <li key={`person-${index}`}>{ person.name }</li>))
+        : null;
+
+    if (!people) {
+        return (
+            <p>There are no people to list</p>
+        );
+    }
+
     return (
-        <>
-        </>
+        <ul>
+            { people }
+        </ul>
     );
 }
 
